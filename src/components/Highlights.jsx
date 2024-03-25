436cc9f7fb0e244549c492f7c5fcbfec4a594de8
finnsatoshi03 <finnsatoshi03@gmail.com>
2024-03-25 23:50:47 +0800

    Add new icons and update Highlights component

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -1,14 +1,53 @@
-function Highlights({ title }) {
+import { useState } from "react";
+
+function Highlights({ title, left = "", top = "", right = "", bottom = "" }) {
   const base = "inline-block rounded-full bg-white px-3 py-2 md:px-4 md:py-2";
 
+  const positionClasses = [
+    left && `left-${left}`,
+    top && `top-${top}`,
+    right && `right-${right}`,
+    bottom && `bottom-${bottom}`,
+  ]
+    .filter(Boolean)
+    .join(" ");
+
+  const [isSubtitleOpen, setSubtitleOpen] = useState(false);
+  const [subtitlePosition, setSubtitlePosition] = useState("");
+
+  const handleMouseEnter = (position) => {
+    setSubtitleOpen(true);
+    setSubtitlePosition(position);
+  };
+
   return (
-    <div className="absolute left-12 top-1/2 z-10 flex gap-4">
-      <span
+    <div
+      className={`absolute ${positionClasses} z-10 flex cursor-pointer gap-4`}
+      onMouseLeave={() => setSubtitleOpen(false)}
+    >
+      <div
         className={`${base} inverted-border-radius-button flex items-center justify-center`}
+        onMouseEnter={() => handleMouseEnter("top-14")}
       >
         <p className="text-sm">{title}</p>
-      </span>
-      <p className={base}>+</p>
+      </div>
+      <div
+        className="rounded-full bg-white px-1.5 py-1"
+        onMouseEnter={() => handleMouseEnter("-right-6 top-14")}
+      >
+        <p className="rounded-full bg-blue-200 px-2.5 py-1">&#10003;</p>
+      </div>
+      {isSubtitleOpen && (
+        <div
+          className={`absolute ${subtitlePosition} flex w-full items-center justify-center rounded-xl bg-black bg-opacity-80 px-6 py-1.5 text-xs text-white`}
+        >
+          {title === "Clean Code*"
+            ? "I highlight efficient coding practices."
+            : title === "Eco UX*"
+              ? "I combine user experience with sustainability"
+              : "I bring your visions to life."}
+        </div>
+      )}
     </div>
   );
 }
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,6 +1,12 @@
 import Socials from "../components/Socials";
 import Highlight from "../components/Highlights";
 
+const highlights = [
+  { title: "Clean Code*", left: "12", top: "2/3" },
+  { title: "Eco UX*", left: "24", top: "1/3" },
+  { title: "Eye Artistry*", right: "10", top: "[30%]" },
+];
+
 function Profile() {
   return (
     <div className="relative" style={{ height: "calc(100vh - 3rem)" }}>
@@ -9,8 +15,18 @@ function Profile() {
         alt="My Profile"
         className="h-full w-full rounded-l-[2.5rem] rounded-tr-[2.5rem] object-cover object-top"
       />
-      <Highlight title="Clean Code*" />
-      {/* <div id="curved-corner-bottomleft"></div> */}
+      {highlights.map((highlight, index) => (
+        <Highlight
+          key={index}
+          title={highlight.title}
+          top={highlight.top}
+          left={highlight.left}
+          right={highlight.right}
+        />
+      ))}
+      {/* <Highlight title="Clean Code*" top="2/3" left="12" />
+      <Highlight title="Eco UX*" top="1/3" left="24" />
+      <Highlight title="Green Design*" top="1/4" right="10" /> */}
       <Socials />
     </div>
   );
