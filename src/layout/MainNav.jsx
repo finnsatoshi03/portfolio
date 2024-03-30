150ac8362e0800b269018e001ecbde74e6394fa5
finnsatoshi03 <finnsatoshi03@gmail.com>
2024-03-30 09:27:45 +0800

    Add sidebar functionality to MainNav component

--- src/layout/MainNav.jsx
+++ src/layout/MainNav.jsx
@@ -1,9 +1,29 @@
+import { useEffect, useRef } from "react";
 import { NavLink } from "react-router-dom";
 
-function MainNav({ sidebar }) {
+function MainNav({ sidebar, setSidebar }) {
+  const sidebarRef = useRef(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (sidebarRef.current && !sidebarRef.current.contains(event.target)) {
+        setSidebar(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [sidebar, setSidebar]);
+
+  const sidebarStyles = `fixed right-0 top-0 z-50 h-full w-[60vw] rounded-s-[3rem] bg-black p-4 transition-transform duration-300 transform ${sidebar ? "translate-x-0" : "translate-x-full"}`;
+
   return (
-    <nav>
-      <ul className={`flex items-center justify-between`}>
+    <nav ref={sidebarRef} className={sidebarStyles}>
+      <ul
+        className={`flex items-center justify-between ${sidebar && "w-full flex-col items-end justify-start text-white"}`}
+      >
         <li className="flex-grow">
           <NavLink to="/home" className="nav-links">
             Home
--- src/layout/Sidebar.jsx
+++ src/layout/Sidebar.jsx
@@ -1,10 +1,15 @@
+import { useState } from "react";
 import MainNav from "./MainNav";
 import Logo from "../components/Logo";
 import useMobileView from "../hooks/useMobileView";
-import Icons from "../assets/icons";
 
 function Sidebar() {
   const isMobile = useMobileView();
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const handleMenuClick = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
 
   return (
     <>
@@ -16,11 +21,15 @@ function Sidebar() {
             width="30"
             src="https://img.icons8.com/material-rounded/300/menu--v1.png"
             alt="menu--v1"
+            onClick={handleMenuClick}
           />
         ) : (
-          <MainNav />
+          <MainNav sidebar={isSidebarOpen} />
         )}
       </div>
+      {isMobile && isSidebarOpen && (
+        <MainNav sidebar={true} setSidebar={setIsSidebarOpen} />
+      )}
     </>
   );
 }
