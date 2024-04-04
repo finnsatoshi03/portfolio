b36b43b7856614e9d917be05538cb5867a049c2b
finnsatoshi03 <finnsatoshi03@gmail.com>
2024-04-04 11:29:23 +0800

    Added some animation in projects page

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,8 +1,30 @@
+import useVisibilityChange from "../hooks/useVisibilityChange";
+
+// Title Component
+const Title = ({ text }) => (
+  <h1 className="relative z-10 font-montserrat text-[5.5rem] font-semibold leading-tight tracking-tighter">
+    {text}
+  </h1>
+);
+
+// Project Component
+const Project = ({ index }) => (
+  <div
+    key={index}
+    className="relative h-[20vh] w-full transition-all duration-300 ease-in-out hover:h-[35vh] hover:cursor-pointer"
+  >
+    <img
+      src="https://placehold.co/600x400"
+      alt=""
+      className="h-full w-full rounded-3xl object-cover"
+    />
+  </div>
+);
+
 function Projects() {
+  useVisibilityChange();
+
   const title = "Sample Long Long long";
-  const elementsBase = "px-6 bg-white text-black";
-  const roundedCorner = "rounded-tr-[2.5rem]";
-  const textStyle = "text-[5.5rem]";
 
   const splitTitle = (title) => {
     let firstPart = "";
@@ -25,13 +47,9 @@ function Projects() {
 
   const [firstPart, secondPart] = splitTitle(title);
 
-  const renderTitle = (text) => (
-    <h1
-      className={`${textStyle} relative z-10 font-montserrat leading-tight tracking-tighter`}
-    >
-      {text}
-    </h1>
-  );
+  const projects = [1, 2, 3, 4].map((_, index) => (
+    <Project key={index} index={index} />
+  ));
 
   return (
     <div className="grid h-full grid-cols-[1fr_0.3fr] gap-3">
@@ -44,32 +62,27 @@ function Projects() {
         />
 
         {/* project logo */}
-        <div
-          className={`${elementsBase} project-logo-inverted-border-top-right absolute left-0 top-0 rounded-br-[2.5rem] py-4 text-xl`}
-        >
+        <div className="project-logo-inverted-border-top-right absolute left-0 top-0 rounded-br-[2.5rem] bg-white px-6 py-4 text-xl text-black">
           <h1 className="relative z-10">Project Logo</h1>
         </div>
 
         {/* title */}
         <div className="title-container absolute bottom-0 left-0 flex flex-col justify-between">
-          <div
-            className={`${elementsBase} title-tag relative w-fit rounded-tr-3xl pt-4`}
-          >
+          <div className="title-tag relative w-fit rounded-tr-3xl bg-white px-6 pt-4 text-black">
             <p className="relative z-10">Sample Tititietie titietietei</p>
           </div>
-          <div
-            className={`${elementsBase} ${roundedCorner} title-h1-p1 relative w-fit`}
-          >
-            {renderTitle(firstPart)}
+          <div className="title-h1-p1 relative w-fit rounded-tr-[2.5rem] bg-white px-6 text-black">
+            <Title text={firstPart} />
           </div>
           {secondPart && (
-            <div
-              className={`${elementsBase} ${roundedCorner} title-h1-p2 relative w-fit`}
-            >
-              {renderTitle(secondPart)}
+            <div className="title-h1-p2 relative w-fit rounded-tr-[2.5rem] bg-white px-6 text-black">
+              <Title text={secondPart} />
             </div>
           )}
         </div>
+
+        {/* inside */}
+        {/* description */}
       </div>
 
       {/* scroller (recent projects) */}
@@ -77,33 +90,14 @@ function Projects() {
         <h1 className="rounded-full border border-black py-2 text-center">
           Project Archives
         </h1>
-        <div className="relative h-[20vh] w-full transition-all duration-300 ease-in-out hover:h-[35vh] hover:cursor-pointer">
-          <img
-            src="https://placehold.co/600x400"
-            alt=""
-            className="h-full w-full rounded-3xl object-cover"
-          />
-        </div>
-        <div className="relative h-[20vh] w-full transition-all duration-300 ease-in-out hover:h-[35vh] hover:cursor-pointer">
-          <img
-            src="https://placehold.co/600x400"
-            alt=""
-            className="h-full w-full rounded-3xl object-cover"
-          />
-        </div>
-        <div className="relative h-[20vh] w-full transition-all duration-300 ease-in-out hover:h-[35vh] hover:cursor-pointer">
-          <img
-            src="https://placehold.co/600x400"
-            alt=""
-            className="h-full w-full rounded-3xl object-cover"
-          />
-        </div>
-        <div className="relative h-[20vh] w-full transition-all duration-300 ease-in-out hover:h-[35vh] hover:cursor-pointer">
-          <img
-            src="https://placehold.co/600x400"
-            alt=""
-            className="h-full w-full rounded-3xl object-cover"
-          />
+        <div className="projects-container max-h-ful">
+          <div
+            className="inner-scroller flex flex-col gap-3"
+            style={{ paddingBlock: "1em" }}
+          >
+            {projects}
+            {projects}
+          </div>
         </div>
       </div>
     </div>
