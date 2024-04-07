17e3e9ef17d7491ba1f463b11059999e3dad6977
finnsatoshi03 <finnsatoshi03@gmail.com>
2024-04-07 18:41:00 +0800

    Refactor AppLayout to display Loader component when isLoading is true

--- src/layout/AppLayout.jsx
+++ src/layout/AppLayout.jsx
@@ -3,13 +3,21 @@ import { Outlet, useLocation } from "react-router-dom";
 import Sidebar from "./Sidebar";
 import Profile from "../pages/Profile";
 import useMobileView from "../hooks/useMobileView";
+import { useProjects } from "../contexts/ProjectContext";
+import Loader from "../components/Loader";
 
 function AppLayout() {
+  const { isLoading } = useProjects();
+  // console.log(isLoading);
+
   const location = useLocation();
   const isMobile = useMobileView();
   const onHome = location.pathname === "/home";
 
+  if (isLoading) return <Loader />;
+
   return (
+    // <Loader />
     <div
       className={`${onHome ? "grid h-screen w-full grid-rows-[1fr_0.5fr] gap-10 xl:grid-cols-[1fr_auto] xl:grid-rows-1" : ""} px-8 py-6`}
     >
